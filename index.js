4a6718992d8e19623ce6d51c81efcbe08bf26a51
Jason Hang <jasonxhang@gmail.com>
2018-06-07 20:16:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return("I can't hear you");
-    else if ()
   }
-}
-
-function sayHiToGrandma(string) {
-  if (string.toUpperCase() === string) {
+    else if (string.toUpperCase() === string) {
     return("YES INDEED!");
+    }
+    else (string === "I love u, Grandma.") {
+      return("I love you, too.");
+    }
   }
-}
+
